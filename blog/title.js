feb497177a4858a9ff661ce95921b37479149c89
johanhombert <johanhombert@users.noreply.github.com>
2020-08-24 14:03:17 +0200

    .

--- blog/title.js
+++ blog/title.js
@@ -1,8 +1,8 @@
 document.write('\
 \
-<p><font face="Arial" size="+3" color="#000066"><b>The Financial Markets Blog</b></font><font face="Verdana" size="+1" color="#EE3300"> by Johan Hombert</font></p>\
+<p><span style="font-family:Arial; font-size:32px; color:#000066; font-weight: bold;">The Financial Markets Blog</span><span style="font-family:Verdana; font-size:18px; color:#EE3300;"> by Johan Hombert</span></p>\
 \
-<p><font face="Verdana" color="#000066">Making sense of financial markets using HEC Paris finance courses</font></p>\
+<p style="font-family:Verdana; color:#000066">Making sense of financial markets using HEC Paris finance courses.</p>\
 \
 <p class="text">Follow on <a href="https://twitter.com/FMblog_jh" target="_blank">Twitter</a> | <a href="https://www.linkedin.com/in/johan-hombert-8467671a7/detail/recent-activity/shares/" target="_blank">LinkedIn</a></p>\
 \
