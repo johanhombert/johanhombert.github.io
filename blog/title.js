18982fc0dca7451ba6f1fbdb968c99a93880a26e
johanhombert <johanhombert@users.noreply.github.com>
2020-09-12 09:13:40 +0200

    .

--- blog/title.js
+++ blog/title.js
@@ -4,7 +4,7 @@ document.write('\
 \
 <p style="font-family:Verdana; color:#000066">Making sense of financial markets using HEC Paris finance courses.</p>\
 \
-<p class="text">Follow on <a href="https://twitter.com/FMblog_jh" target="_blank">Twitter</a> | <a href="https://www.linkedin.com/in/johan-hombert-8467671a7/detail/recent-activity/shares/" target="_blank">LinkedIn</a></p>\
+<p class="text">Follow on <a href="https://www.linkedin.com/in/johan-hombert-8467671a7/detail/recent-activity/shares/" target="_blank">LinkedIn</a> | <a href="https://twitter.com/FMblog_jh" target="_blank">Twitter</a></p>\
 \
 <p>&nbsp;</p>\
 \
