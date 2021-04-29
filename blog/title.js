26767ef683857f4a755e4c606053536661e0912a
johanhombert <johanhombert@users.noreply.github.com>
2021-04-29 08:54:38 +0200

    .

--- blog/title.js
+++ blog/title.js
@@ -1,10 +1,10 @@
 document.write('\
 \
-<p><span style="font-family:Arial; font-size:32px; color:#000066; font-weight: bold;">The Finance Blog</span><span style="font-family:Verdana; font-size:24px; color:#EE3300;"> by Johan Hombert</span></p>\
+<p><span style="font-family:Arial; font-size:32px; color:#000066; font-weight: bold">The Finance Blog</span><span style="font-family:Verdana; font-size:24px; color:#EE3300"> by Johan Hombert</span></p>\
 \
-<p style="font-family:Verdana; color:#000066">Making sense of finance using academic ideas.</p>\
+<p style="font-family:Verdana; font-size:16px; color:#000066">Making sense of finance using HEC Paris courses and academic ideas.</p>\
 \
-<p class="text">Follow on <a href="https://www.linkedin.com/in/johan-hombert-8467671a7/detail/recent-activity/shares/" target="_blank">LinkedIn</a> | <a href="https://twitter.com/FMblog_jh" target="_blank">Twitter</a></p>\
+<p style="font-family:Arial; font-size:14px">Follow on <a href="https://www.linkedin.com/in/johan-hombert-8467671a7/detail/recent-activity/shares/" target="_blank">LinkedIn</a> | <a href="https://twitter.com/FMblog_jh" target="_blank">Twitter</a></p>\
 \
 <p>&nbsp;</p>\
 \
