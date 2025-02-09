e5e9f282323693b8c3a4cafbb716d2d8ce5b0bdc
johanhombert <62036566+johanhombert@users.noreply.github.com>
2025-02-09 17:31:39 +0100

    Update title.js

--- blog/title.js
+++ blog/title.js
@@ -4,7 +4,7 @@ document.write('\
 \
 <p style="font-family:Verdana; font-size:16px; color:#000066">Connecting HEC Paris courses, academic ideas &#38; real world problems</p>\
 \
-<p style="font-family:Arial; font-size:14px">Follow on <a href="https://www.linkedin.com/in/johan-hombert-8467671a7/detail/recent-activity/shares/" target="_blank">LinkedIn</a> | <a href="https://twitter.com/FMblog_jh" target="_blank">Twitter</a> | <a href="https://www.instagram.com/johanhombert/" target="_blank">Instagram</a></p>\
+<p style="font-family:Arial; font-size:14px">Follow on <a href="https://www.linkedin.com/in/johan-hombert-8467671a7/detail/recent-activity/shares/" target="_blank">LinkedIn</a> | <a href="https://twitter.com/FMblog_jh" target="_blank">Twitter</a></p>\
 \
 <p>&nbsp;</p>\
 \
